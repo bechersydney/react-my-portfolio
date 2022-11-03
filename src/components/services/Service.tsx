51697ec8a2c5d6f3021d269e2ed6fd07791287bb
sydney <becher.sydney@gmail.com>
2022-11-03 12:58:52 +0800

    added services page

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,6 +1,164 @@
-import classes from "./services.module.css";
+import "./services.css";
+import { CheckOutlined } from "@mui/icons-material";
 
 const Service = () => {
-    return <section id="services">services</section>;
+    return (
+        <section id="services">
+            <h5>What I offer?</h5>
+            <h2>Services</h2>
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <CheckOutlined fontSize="small"  className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF UI/UX DESIGN */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF WEB DEV */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Vlogging</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                        <li>
+                            <CheckOutlined fontSize="small" className="service__list-icon" />
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Atque, reprehenderit!
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF VLOGGING */}
+            </div>
+        </section>
+    );
 };
 export default Service;

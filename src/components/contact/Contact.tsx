ed2d90656c3f463b277c65c3f96618631b854db9
sydney <becher.sydney@gmail.com>
2022-11-06 23:06:34 +0800

    added initial contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,5 +1,82 @@
-import classes from "./contact.module.css";
+import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
+
 const Contact = () => {
-    return <section id="contact">contact</section>;
+    const [error, setError] = useState('');
+    const submitEmailMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const response = await emailjs.sendForm(
+            "service_zhn0asu",
+            "template_hj87fzf",
+            e.currentTarget,
+            "OgXM0FUDcOjnWDoqm"
+        );
+        if (response.text !== 'OK')  setError("Error sending email");
+    };
+    return (
+        <section id="contact">
+            <h5>Get in touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option-item">
+                        <MdOutlineMail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5 className="text-light">becher.sydney@gmail.com</h5>
+                        <a
+                            href="mailto:becher.sydney@gmail.com"
+                            target="_blank"
+                        >
+                            Send a message
+                        </a>
+                    </article>
+                    <article className="contact__option-item">
+                        <FaFacebookMessenger className="contact__option-icon" />
+                        <h4>Messenger</h4>
+                        <h5 className="text-light">sydney_becher</h5>
+                        <a href="https://m.me/sydney.becher" target="_blank">
+                            Send a message
+                        </a>
+                    </article>
+                    <article className="contact__option-item">
+                        <AiFillMessage className="contact__option-icon" />
+                        <h4>Messaging</h4>
+                        <h5 className="text-light">+639318487909</h5>
+                        <a href="" target="_blank">
+                            Send a message
+                        </a>
+                    </article>
+                </div>
+                <form onSubmit={submitEmailMessage}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Full Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your  Email"
+                        required
+                    />
+                    <textarea
+                        placeholder="Your Message"
+                        name="message"
+                        rows={7}
+                        required
+                    />
+                    <button type="submit" className="btn btn-primary">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
 };
 export default Contact;
--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,6 +1,107 @@
-import classes from "./testimonial.module.css";
+import "./testimonials.css";
+import me from "../../assets/images/me.png";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+    {
+        id: 1,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+    {
+        id: 2,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+    {
+        id: 3,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+    {
+        id: 4,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+    {
+        id: 5,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+    {
+        id: 6,
+        avatar: me,
+        name: "Sydney Becher",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.\
+    Amet sed debitis quis consequuntur reiciendis accusantium\
+     ipsam tempora labore delectus, molestias maiores enim dolores ab similique eum. Consequatur quia \
+     delectus, molestias maiores enim dolores ab similique\
+     eum. Consequatur quia mollitia praesentium.\
+        mollitia praesentium. accusantium ipsam tempora labore",
+    },
+];
 
 const Testimonials = () => {
-    return <></>;
+    return (
+        <section id="testimonials">
+            <h5>Review for clients</h5>
+            <h2>Testimonials</h2>
+            <Swiper
+                className="container testimonial__container"
+                modules={[Pagination]}
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {data.map(({ id, avatar, name, review }) => (
+                    <SwiperSlide className="testimonial" key={id}>
+                        <div className="client__avatar">
+                            <img src={avatar} alt="" />
+                        </div>
+                        <h5 className="client__name">{name}</h5>
+                        <small className="client__review">{review}</small>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
 };
 export default Testimonials;
